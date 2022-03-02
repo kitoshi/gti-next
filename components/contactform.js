44cdbf668f25a7052a7b4de57784ecd8e802afa3
kitoshi <kitoshi.charlton@gmail.com>
2022-03-02 11:05:06 -0800

    calendly add

--- components/contactform.js
+++ components/contactform.js
@@ -1,24 +1,15 @@
 import React from 'react'
-import { useForm, ValidationError } from '@formspree/react'
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm('xyyogogy')
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>
-  }
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ display: 'flex', flexDirection: 'column' }}
-    >
-      <label htmlFor='email'>Email Address</label>
-      <input id='email' type='email' name='email' />
-      <ValidationError prefix='Email' field='email' errors={state.errors} />
-      <label htmlFor='message'>Message</label>
-      <textarea id='message' name='message' />
-      <ValidationError prefix='Message' field='message' errors={state.errors} />
-      <button type='submit' disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <>
+      <div style={{ height: '800px' }}>
+        <iframe
+          src='https://calendly.com/kitoshi-charlton/'
+          width='100%'
+          height='100%'
+          frameBorder='0'
+        ></iframe>
+      </div>
+    </>
   )
 }

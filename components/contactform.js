375b85f4d755e8561e629812bcaa4475a59cb2cc
kitoshi <kitoshi.charlton@gmail.com>
2022-03-03 15:30:33 -0800

    calendly popup removed

--- components/contactform.js
+++ components/contactform.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
 export default function ContactForm() {
+  useEffect(() => {
+    const head = document.querySelector('head')
+    const script = document.createElement('script')
+    script.setAttribute(
+      'src',
+      'https://assets.calendly.com/assets/external/widget.js'
+    )
+    head.appendChild(script)
+  }, [])
+
   return (
-    <>
-      <div style={{ height: '800px' }}>
-        <iframe
-          src='https://calendly.com/g-t-i/'
-          width='100%'
-          height='100%'
-          frameBorder='0'
-          title='Calender meeting service'
-        ></iframe>
-      </div>
-    </>
+    <div
+      className='calendly-inline-widget'
+      data-url='https://calendly.com/g-t-i?hide_landing_page_details=1&hide_gdpr_banner=1'
+      style={{ minWidth: '320px', height: '630px' }}
+    />
   )
 }
